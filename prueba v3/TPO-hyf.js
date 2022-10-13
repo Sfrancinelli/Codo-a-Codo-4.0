688721387005d8f9fa9a3dc7bd51e8400189cae4
Sebastián Francinelli <112777037+Sfrancinelli@users.noreply.github.com>
2022-10-13 09:50:34 -0300

    Add files via upload
    Agregada la verificación de formulario de contacto a esta branch.

--- prueba v3/TPO-hyf.js
+++ prueba v3/TPO-hyf.js
@@ -46,7 +46,7 @@ document.getElementById("encabezado").innerHTML = `
     <div class="top-navbar">
         <div class="item1">
             <img src="img/direccion.svg" alt="Icon Dirección" class="item-icon1">
-            <span class="direccion"><a href="https://www.google.com.ar/maps/place/Florida+325+c1005,+C1005AAG+CABA/@-34.6042749,-58.3761541,18z/data=!3m1!4b1!4m5!3m4!1s0x95bccacda4c17dc9:0x75b394bd29700b01!8m2!3d-34.6042771!4d-58.3750598" target="_blank">Florida 325</a></span>
+            <span class="direccion">Florida 325</span>
         </div>
         <div class="item2">
             <img src="img/telefono.svg" alt="Icon Teléfono" class="item-icon2">
@@ -171,3 +171,44 @@ document.getElementById("pie").innerHTML = `
                     <small>Derechos reservados @2022</small>
                 </div>
 `
+
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	dni: /^.{7,8}$/, // 7 a 8 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
+
+$(document).ready(function() {
+
+    $('#formulario').submit(function(e) {
+      e.preventDefault();
+      
+      // OBTENER LOS DATOS. 
+      var nombre = $('#input_nombre').val();
+      var surname = $('#input_apellido').val();
+      var DNI = $('#input_DNI').val();
+      var cel = $('#input_celular').val();
+      var email = $('#input_email').val();
+      //Guardo los mensajes de error en un array para mostrarlos secuencialmente
+      var errores = [];
+
+      if (!expresiones.nombre.test(nombre) || !expresiones.nombre.test(surname))
+        errores.push('Nombre y apellido solo pueden contener letras, espacios y acentos');
+      if (!expresiones.dni.test(DNI))
+        errores.push('El DNI puede tener 7 a 8 digitos');
+      if (!expresiones.telefono.test(cel))
+        errores.push('El nro de celular puede tener de 7 a 14 numeros');
+      if (!expresiones.correo.test(email))
+        errores.push('El email deber respetar el formato xxx@xxx.xx');
+
+      // Si hay errores los muestro, de lo contrario reseteo el formulario
+      if (errores.length > 0) {
+        $("div.msg_error").html(errores.join(' | '))}
+      else {
+        $('#formulario').get(0).reset();
+        $("div.msg_error").html('El formulario se envio correctamente')}
+    });
+    
+});
