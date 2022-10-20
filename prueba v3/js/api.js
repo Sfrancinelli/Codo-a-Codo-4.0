b4fdb9daba274f7975cceec3e7d0a526c42fb705
Sebastián Francinelli <112777037+Sfrancinelli@users.noreply.github.com>
2022-10-20 17:32:35 -0300

    API consumida en novedades.html falta CSS

--- prueba v3/js/api.js
+++ prueba v3/js/api.js
@@ -1,20 +1,61 @@
-const {createApp} = Vue
+// const {createApp} = Vue
+
+// createApp ({
+//     data() {
+//         return {
+//             url: 'https://mocki.io/v1/58dce575-7d57-413d-a768-07c249bf850c', //fake api creada por mí
+//             noticias: []
+//         }
+//     },
+//     methods: {
+//         async fetchData(url) {
+//             const res = await fetch(url);
+//             data = await res.json();
+//             this.noticias = data.results;
+//         }
+//     },
+//     async created() {
+//         await this.fetchData(this.url);
+//     }
+// }).mount('#app');
+
+// const {createApp} = Vue;
+
+// createApp ({
+//     data() {
+//         return {
+//             url:'https://mocki.io/v1/f807fc94-68b6-442d-ad34-5b869ced8412',
+//             covid:[]
+//         }
+//     },
+//     methods: {
+//         async fetchData(url) {
+//             const res = await fetch(url);
+//             data = await res.json;
+//             this.covid = data.results;
+//         }
+//     },
+//     async created() {
+//         await this.fetchData(this.url)
+//     }
+// }).mount('#app')
+
+
+const {createApp} = Vue 
 
 createApp ({
     data() {
-        return {
-            url: 'https://mocki.io/v1/58dce575-7d57-413d-a768-07c249bf850c', //fake api creada por mí
-            noticias: []
+        return{
+            info: {} 
         }
     },
-    methods: {
-        async fetchData(url) {
-            const res = await fetch(url);
-            data = await res.json();
-            this.noticias = data.results;
-        }
-    },
-    async created() {
-        await this.fetchData(this.url);
+    created() {
+        fetch('https://mocki.io/v1/f807fc94-68b6-442d-ad34-5b869ced8412')
+        .then(response => response.json())
+        .then(data => { 
+            console.log(data);
+            this.info=data
+            })
+        .catch( error => console.log(error));
     }
-}).mount('#app');
+}).mount('#app');;
