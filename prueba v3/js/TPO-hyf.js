153053e7b09a11aeda939d0dd37e07865d738f2e
Sebastián Francinelli <112777037+Sfrancinelli@users.noreply.github.com>
2022-10-20 10:44:06 -0300

    Agregado modal de Fernando. Falta alinear hamburguesa

--- prueba v3/js/TPO-hyf.js
+++ prueba v3/js/TPO-hyf.js
@@ -1,130 +1,170 @@
 document.getElementById("encabezado").innerHTML = `
 <div class="logo-container">
-    <a class="logo" href="index.html">
-        <img src="img/LOGO-PRUEBA.png" alt="Logo TPO">
-    </a>
-</div>
-
-<div class="resto-header"> 
-    <div class="nav-bar">
-        <nav class="nav">
-            <ul class="inicio">
-                <li class="activa">
-                    <a aria-current="page" class="active" href="index.html">INICIO </a>
-                </li>
-                <li>
-                    <a href="institucional.html">INSTITUCIONAL </a>
-                </li>
-                <li>
-                    <a href="asistencia.html">ASISTENCIA AL PACIENTE </a>
-                </li>
-                <li>
-                    <a href="novedades.html">NOVEDADES </a>
-                </li>
-                <li>
-                    <a href="contacto.html">CONTACTO </a>
-                </li>
-            </ul>
-        </nav>
-    </div>
-
-    <div class="icons-nav-832">
-        <h2>Más información:</h2>
-        <div>
-            <a class="redsoc" href="http://www.facebook.com" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
-            <a class="redsoc" href="http://www.twitter.com" target="_blank"><i class="fa-brands fa-twitter"></i></a>
-            <a class="redsoc" href="http://www.instagram.com" target="_blank"><i class="fa-brands fa-square-instagram" style="font-size: 1.05em;"></i></a>
-            <a class="redsoc" target="_blank" href="mailto:ejemplo@gmail.com"><i class="fa-solid fa-envelope"></i></a>
-        </div>                      
-    </div>
-
-</div>
-    <div class="top-navbar-responsive">
-        <h1>Centro Médico "Codo a Codo"</h1>
-    </div>
-
-    <div class="top-navbar">
-        <div class="item1">
-            <img src="img/direccion.svg" alt="Icon Dirección" class="item-icon1">
-            <span class="direccion">Florida 325</span>
-        </div>
-        <div class="item2">
-            <img src="img/telefono.svg" alt="Icon Teléfono" class="item-icon2">
-            <span class="telefono">4581-8538</span>
-        </div>
-        <div class="item3">
-            <img src="img/whatsapp.svg" alt="Icon Whatsapp" class="item-icon3">
-            <span class="whatsapp">11 4805-7548</span>
-        </div>
-        <div class="item4">
-            <img src="img/horario.svg" alt="Icon Horario" class="item-icon4">
-            <span class="horario">Lun a Vie:8 a 20hs.</span>
-        </div>
-    </div>
-
-    <div class="flex-log-icon">
-        <div class="log-in" onclick="document.getElementById('logear').style.display='block'">
-            <a rel="noopener noreferrer" href="#" class="boton-log">Log in</a>
-    </div>
-
-    <div id="logear" class="modal">
-        <form class="modal-contenido animate" action="#" method="post">
-            <div class="container">
-                <label for="usuario"><b>CUIT/CUIL</b></label>
-                <input type="text" placeholder="Introduzca su CUIT/CUIL..." name="cuit" required>                      
-                <label for="contra"><b>Contraseña</b></label>
-                <input type="password" placeholder="Introduzca su contraseña..." name="contra" required>                              
-                <button type="submit">Login</button>
-                <label>
-                <input type="checkbox" checked="checked" name="recordar"> Recordarme
-                </label>
-                <div class="register"><p>¿Todavía no tiene cuenta? <a href="#">Registrarse</a></p></div>
-            </div>                      
-            <div class="container" style="background-color:#f1f1f1">
-                <button type="button" onclick="document.getElementById('logear').style.display='none'" class="cancelarbtn">Cancelar</button>
+                <a class="logo" href="index.html">
+                    <img src="img/LOGO-PRUEBA.png" alt="Logo TPO">
+                </a>
             </div>
-        </form>
-    </div> 
 
-    <script>
-        var modal = document.getElementById('logear'); 
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
-    </script> 
+            <div class="resto-header"> 
+                <div class="nav-bar">
+                    <nav class="nav">
+                        <ul class="inicio">
+                            <li class="activa">
+                                <a aria-current="page" class="active" href="index.html">INICIO </a>
+                            </li>
+                            <li>
+                                <a href="institucional.html">INSTITUCIONAL </a>
+                            </li>
+                            <li>
+                                <a href="asistencia.html">ASISTENCIA AL PACIENTE </a>
+                            </li>
+                            <li>
+                                <a href="novedades.html">NOVEDADES </a>
+                            </li>
+                            <li>
+                                <a href="contacto.html">CONTACTO </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
 
-    <div class="icons-nav">
-        <a class="redsoc" href="http://www.facebook.com" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
-        <a class="redsoc" href="http://www.twitter.com" target="_blank"><i class="fa-brands fa-twitter"></i></a>
-        <a class="redsoc" href="http://www.instagram.com" target="_blank"><i class="fa-brands fa-square-instagram"></i></a>
-        <a class="redsoc" target="_blank" href="mailto:ejemplo@gmail.com"><i class="fa-solid fa-envelope"></i></a>
-    </div>
-    </div>
+                <div class="icons-nav-832">
+                    <h2>Más información:</h2>
+                    <div>
+                        <a class="redsoc" href="http://www.facebook.com" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a class="redsoc" href="http://www.twitter.com" target="_blank"><i class="fa-brands fa-twitter"></i></a>
+                        <a class="redsoc" href="http://www.instagram.com" target="_blank"><i class="fa-brands fa-square-instagram" style="font-size: 1.05em;"></i></a>
+                        <a class="redsoc" target="_blank" href="mailto:ejemplo@gmail.com"><i class="fa-solid fa-envelope"></i></a>
+                    </div>                      
+                </div>
 
-    <div class="hamburguesa">
-        <span class="ham"></span>
-        <nav class="p-n" id="p-n">
-        <ul>
-            <li class="activa">
-                <a aria-current="page" class="active" href="index.html">INICIO</a>
-            </li>
-            <li>
-                <a href="institucional.html">INSTITUCIONAL </a>
-            </li>
-            <li>
-                <a href="asistencia.html">ASISTENCIA AL PACIENTE </a>
-            </li>
-            <li>
-                <a href="novedades.html">NOVEDADES </a>
-            </li>
-            <li>
-                <a href="contacto.html">CONTACTO </a>
-            </li>
-        </ul>
-        </nav>                        
-    </div>         
+            </div>
+                <div class="top-navbar-responsive">
+                    <h1>Centro Médico "Codo a Codo"</h1>
+                </div>
+
+                <div class="top-navbar">
+                    <div class="item1">
+                        <img src="img/direccion.svg" alt="Icon Dirección" class="item-icon1">
+                        <span class="direccion"><a href="https://www.google.com.ar/maps/place/Florida+325+c1005,+C1005AAG+CABA/@-34.6042749,-58.3761541,18z/data=!3m1!4b1!4m5!3m4!1s0x95bccacda4c17dc9:0x75b394bd29700b01!8m2!3d-34.6042771!4d-58.3750598" target="_blank">Florida 325</a></span>
+                    </div>
+                    <div class="item2">
+                        <img src="img/telefono.svg" alt="Icon Teléfono" class="item-icon2">
+                        <span class="telefono">4581-8538</span>
+                    </div>
+                    <div class="item3">
+                        <img src="img/whatsapp.svg" alt="Icon Whatsapp" class="item-icon3">
+                        <span class="whatsapp">11 4805-7548</span>
+                    </div>
+                    <div class="item4">
+                        <img src="img/horario.svg" alt="Icon Horario" class="item-icon4">
+                        <span class="horario">Lun a Vie:8 a 20hs.</span>
+                    </div>
+                </div>
+
+                <div class="flex-log-icon">
+                    <button id="btn-iniciar-sesion" data-modal-objetivo="#modal-iniciar-sesion"
+                    class="btn-modal-abrir boton-menu">
+                    Log in
+                    </button>
+                    <div class="contenedor-modal" id="contenedor-modal-1">
+                        <div class="modal modal-generico" id="modal-iniciar-sesion">
+                            <div class="modal-header">
+                                <h5 class="modal-titulo">Iniciar sesión</h5>
+                                <button class="btn-modal-cerrar">X</button>
+                            </div>
+                            <div class="modal-body">
+                                <form action="#" method="post">
+                                    <fieldset class="label-textbox">
+                                        <label for="cuit_iniciar_sesion">CUIT/CUIL (sin guiones)</label>
+                                        <input id="cuit_iniciar_sesion" name="cuit" type="number"
+                                            min="1" placeholder="Introduzca su CUIT/CUIL..." required>
+                                    </fieldset>
+                                    <fieldset class="label-textbox">
+                                        <label for="contraseña_iniciar_sesion">Contraseña</label>
+                                        <input id="contraseña_iniciar_sesion" name="contraseña" type="password"
+                                            placeholder="Introduzca su contraseña..." required>
+                                    </fieldset>
+                                    <button type="submit">Iniciar sesión</button>
+                                    <fieldset class="label-checkbox">
+                                        <label for="recordar">Recordarme</label>
+                                        <input id="recordar" type="checkbox" checked="checked">
+                                    </fieldset>
+                                    <a role="button" href="#" data-modal-objetivo="#modal-registrarse" class="btn-modal-abrir">¿Todavía no
+                                        tienes una cuenta?</a>
+                                </form>
+                            </div>
+                        </div>
+
+                        <div class="modal modal-generico" id="modal-registrarse">
+                            <div class="modal-header">
+                                <h5 class="modal-titulo">Registrarse</h5>
+                                <button class="btn-modal-cerrar">X</button>
+                            </div>
+                            <div class="modal-body">
+                                <form action="#" method="post">
+                                    <fieldset class="label-textbox">
+                                        <label for="nombre_registrarse">Nombre</label>
+                                        <input id="nombre_registrarse" name="nombre" type="text"
+                                            placeholder="Introduzca su nombre..." required>
+                                    </fieldset>
+                                    <fieldset class="label-textbox">
+                                        <label for="apellido_registrarse">Apellido</label>
+                                        <input id="apellido_registrarse" name="apellido" type="text"
+                                            placeholder="Introduzca su apellido..." required>
+                                    </fieldset>
+                                    <fieldset class="label-textbox">
+                                        <label for="dni_registrarse_registrarse">DNI</label>
+                                        <input id="dni_registrarse" name="dni" type="number"
+                                        min="1" placeholder="Introduzca su DNI..." required>
+                                    </fieldset>
+                                    <fieldset class="label-textbox">
+                                        <label for="cuit_registrarse">CUIT/CUIL (sin guiones)</label>
+                                        <input id="cuit_registrarse" name="cuit" type="number"
+                                        min="1" placeholder="Introduzca su CUIT/CUIL..." required>
+                                    </fieldset>
+                                    <fieldset class="label-textbox">
+                                        <label for="contraseña_registrarse">Contraseña</label>
+                                        <input id="contraseña_registrarse" name="contraseña" type="password"
+                                            placeholder="Introduzca su contraseña..." required>
+                                    </fieldset>
+                                    <button type="submit">Registrarse</button>
+                                    <a role="button" href="#" data-modal-objetivo="#modal-iniciar-sesion" class="btn-modal-abrir">¿Ya
+                                        tienes una cuenta?</a>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="icons-nav">
+                        <a class="redsoc" href="http://www.facebook.com" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a class="redsoc" href="http://www.twitter.com" target="_blank"><i class="fa-brands fa-twitter"></i></a>
+                        <a class="redsoc" href="http://www.instagram.com" target="_blank"><i class="fa-brands fa-square-instagram"></i></a>
+                        <a class="redsoc" target="_blank" href="mailto:ejemplo@gmail.com"><i class="fa-solid fa-envelope"></i></a>
+                    </div>
+                </div>
+
+                <div class="hamburguesa">
+                    <span class="ham"></span>
+                    <nav class="p-n" id="p-n">
+                    <ul>
+                        <li class="activa">
+                            <a aria-current="page" class="active" href="index.html">INICIO</a>
+                        </li>
+                        <li>
+                            <a href="institucional.html">INSTITUCIONAL </a>
+                        </li>
+                        <li>
+                            <a href="asistencia.html">ASISTENCIA AL PACIENTE </a>
+                        </li>
+                        <li>
+                            <a href="novedades.html">NOVEDADES </a>
+                        </li>
+                        <li>
+                            <a href="contacto.html">CONTACTO </a>
+                        </li>
+                    </ul>
+                    </nav>                        
+                </div>         
 `
 document.getElementById("pie").innerHTML = `
 <div class="logo-footer">
